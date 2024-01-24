054b553a24ddae52de1636b4a29121398f6bcdfc
In28 <ina.moisejeva@gmail.com>
2024-01-24 12:30:32 +0200

    tvarkytas CheckIn

--- src/Components/CheckIn/CheckIn.jsx
+++ src/Components/CheckIn/CheckIn.jsx
@@ -5,6 +5,7 @@ import { MdAddCall } from "react-icons/md";
 import { RiMailAddLine } from "react-icons/ri";
 import { MdDelete } from "react-icons/md";
 import { SiYourtraveldottv } from 'react-icons/si';
+import { IoCheckmarkDoneOutline } from "react-icons/io5";
 import { Link } from 'react-router-dom';
 
 const CheckIn = () => {
@@ -21,80 +22,98 @@ const CheckIn = () => {
 }
 
 const CheckInForm = () => {
-  const [name, setName] = useState('')
-  const [phone, setPhone] = useState('')
-  const [email, setEmail] = useState('')
-  const [entries, setEntries] = useState([])
+  const [date, setDate] = useState('');
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [entries, setEntries] = useState([]);
+
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  }
 
   const handleNameChange = (e) => {
-    setName(e.target.value)
+    setName(e.target.value);
   }
 
   const handlePhoneChange = (e) => {
-    setPhone(e.target.value)
+    setPhone(e.target.value);
   }
 
   const handleEmailChange = (e) => {
-    setEmail(e.target.value)
+    setEmail(e.target.value);
   }
 
   const handleAddEntry = () => {
     // El. pašto tikrinimas
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (name.trim() !== '' && phone.trim() !== '' && emailRegex.test(email.trim())) {
-      setEntries([...entries, { name, phone, email }])
-      setName('')
-      setPhone('')
-      setEmail('')
+    if (date.trim() !== '' && name.trim() !== '' && phone.trim() !== '' && emailRegex.test(email.trim())) {
+      setEntries([...entries, { date, name, phone, email }]);
+      setDate('');
+      setName('');
+      setPhone('');
+      setEmail('');
     } else {
-      alert('Įvestas el. pašto adresas netinkamo formato!')
+      alert("Įvestas el. pašto adresas netinkamo formato!");
     }
   }
 
   const handleRemoveEntry = (index) => {
-    const updatedEntries = [...entries]
-    updatedEntries.splice(index, 1)
-    setEntries(updatedEntries)
+    const updatedEntries = [...entries];
+    updatedEntries.splice(index, 1);
+    setEntries(updatedEntries);
   }
 
   return (
     <div className="flex-container">
       <h2>Check-In Form</h2>
-      <div>
-        <IoMdPersonAdd />
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={handleNameChange}
-        />
-        <MdAddCall />
-        <input
-          type="number"
-          placeholder="Phone"
-          value={phone}
-          onChange={handlePhoneChange}
-        />
-        <RiMailAddLine />
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <button onClick={handleAddEntry}><IoAddOutline /></button>
-      </div>
+        <div>
+          <button className='inputBtn'><input
+            type="date"
+            placeholder="date"
+            value={date}
+            onChange={handleDateChange}
+          /></button>
+          
+          <button className='inputBtn'><input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={handleNameChange}
+          /><IoMdPersonAdd /></button>
+          
+          <button className='inputBtn'> <input
+            type="number"
+            placeholder="Phone"
+            value={phone}
+            onChange={handlePhoneChange}
+          /><MdAddCall /></button>
+          
+          <button className='inputBtn'><input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={handleEmailChange}
+          /><RiMailAddLine /></button>
+          
+          <button className='btnAdd' onClick={handleAddEntry}><IoAddOutline /></button>
+        </div>
       <ul>
         {entries.map((entry, index) => (
           <li key={index}>
-            {entry.name}, {entry.phone}, {entry.email}
-            <button onClick={() => handleRemoveEntry(index)}><MdDelete /></button>
+            {entry.date},  {entry.name}, {entry.phone}, {entry.email}
+            <button className='btnDel' onClick={() => handleRemoveEntry(index)}><MdDelete /></button>
           </li>
         ))}
       </ul>
+
+      <button className='btn'>
+          <Link to="/submit">Submit<IoCheckmarkDoneOutline  className='icon' /></Link>  
+        </button>
     </div>
   )
 }
 
-export default CheckIn
+export default CheckIn;
+
 
