4a478644825fc566eaf4a99b5178a74af2f6f799
In28 <ina.moisejeva@gmail.com>
2024-01-25 10:44:25 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,18 @@ import GetStart from './Components/GetStart/GetStart';
 import CheckIn from './Components/CheckIn/CheckIn';
 import Submit from './Components/Submit/Submit';
 
+import Booking from './Components/Booking/Booking';
+import './Components/Booking/_booking.scss'
+
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/get-start" element={<GetStart />} />
-        
+        <Route path="/booking" element={<Booking />} />
+
         <Route path="/check-in" element={<CheckIn />} />
         <Route path="/submit" element={<Submit />} />
       </Routes>
---
+++ src/Components/Booking/Booking.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Booking = () => {
+  return (
+    <h1>tai ka tu?</h1>
+  )
+}
+
+export default Booking
